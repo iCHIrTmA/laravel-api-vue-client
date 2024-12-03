817b2853fbddfc5543a4ac545fb6fa9628b7c2a9
iCHIrTmA <jonrue.dev@gmail.com>
2024-12-03 10:50:15 +0800

    feat: basic authentication

--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -1,8 +1,13 @@
 import './assets/main.css'
+import axios from 'axios'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
+axios.defaults.baseURL = 'http://localhost:8000'
+axios.defaults.withCredentials = true
+axios.defaults.withXSRFToken = true
 
 const app = createApp(App)
 
--- vue-client/src/views/HomeView.vue
+++ vue-client/src/views/HomeView.vue
@@ -1,9 +1,34 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+  import axios from 'axios'
+  import { reactive } from 'vue'
+
+  const form = reactive({
+    email: 'janroe.dev@example.net',
+    password: 'P@ssw0rd',
+  });
+
+  const login = async () => {
+    await axios.get('/sanctum/csrf-cookie')
+    await axios.post('/login', form)
+
+    axios.get('/api/user').then((response) => {
+      console.log(response)
+    })
+  }
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <form v-on:submit.prevent="login">
+      <div>
+        <label for="email">Email</label>
+        <input type="text" id="email" v-model="form.email">
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input type="text" id="password" v-model="form.password">
+      </div>
+      <button type="submit">Log in</button>
+    </form>
   </main>
 </template>
